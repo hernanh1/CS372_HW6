dd5d67f193bd1f3ac85edfefd427f98663a95526
Henry <hernan_h1@denison.edu>
2022-11-06 21:11:13 -0500

    Added some changes to clone. They are untested

--- sysproc.c
+++ sysproc.c
@@ -93,7 +93,51 @@ sys_uptime(void)
 int
 sys_clone(void)
 {
-  return 420;
+  int i, pid;
+  struct proc *np;
+  struct proc *curproc = myproc();
+
+  // Allocate process.
+  if((np = allocproc()) == 0){
+    return -1;
+  }
+
+  /*
+  // Copy process state from proc.
+  if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
+    kfree(np->kstack);
+    np->kstack = 0;             //np->kstack should equal curproc->kstack, as they need to share the same address space.
+    np->state = UNUSED;
+    return -1;
+  }
+  */
+
+  np->state = UNUSED;
+  np->pgdir = curproc->pgdir; //Both threads should share the same Pgdir, as they need to share the same data.
+  np->kstack = curproc->kstack; //Both threads should share the same kstack.
+
+  np->sz = curproc->sz;
+  np->parent = curproc;
+  *np->tf = *curproc->tf;
+
+  // Clear %eax so that fork returns 0 in the child.
+  np->tf->eax = 0;
+  //something about eip?
+  //something about tf->esp?
+
+  for(i = 0; i < NOFILE; i++)
+    if(curproc->ofile[i])
+      np->ofile[i] = filedup(curproc->ofile[i]);
+  np->cwd = idup(curproc->cwd);
+
+  safestrcpy(np->name, curproc->name, sizeof(curproc->name));
+  pid = np->pid;
+
+  acquire(&ptable.lock);
+  np->state = RUNNABLE;
+  release(&ptable.lock);
+
+  return pid;
 }
 
 int sys_join(void)
